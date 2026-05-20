32dbc160eabe3d04bb052fc248d15d4a4df418ac
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-05-20 15:17:12 +0200

    Update site-nowplaying.js

--- radios/site-nowplaying.js
+++ radios/site-nowplaying.js
@@ -55,10 +55,10 @@ async function getNowPlayingGrupaZPR(stationId) {
       }
       const data = await response.json();
       // dostosuj do rzeczywistej struktury API
-      const current = data.raw.current || data.current || data;
-      if (data.raw.current !== null) {
-         const artist = current.artist || 'Nieznany artysta';
-         const title = current.title || 'Nieznany utwór';
+      const current = data.current || data;
+      if (data.current !== null) {
+         const artist = current.artists || 'Nieznany artysta';
+         const title = current.name || 'Nieznany utwór';
 
          container.innerHTML = `<h4>Teraz gramy:</h4><small>${artist} - ${title}</small>`;
       } else {
