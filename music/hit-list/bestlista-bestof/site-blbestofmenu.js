278124eb96dba04bc44e4aad2f87c1f04e3946cc
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-05-20 20:10:34 +0200

    Add files via upload

--- music/hit-list/bestlista-bestof/site-blbestofmenu.js
+++ music/hit-list/bestlista-bestof/site-blbestofmenu.js
@@ -1,1 +1,1 @@
-document.write('<div id=\"menusub\"><div id=\"menuranking\"><div class=\"menulist\"><a href=\"https://krdrt5370000ym.github.io/music/hit-list/bestlista-bestof/2025\">2025</a></div></div></div>');
+document.write('<div id=\"menusub\"><div id=\"menuranking\"><div class=\"menulist\"><a href=\"https://krdrtradio.github.io/music/hit-list/bestlista-bestof/2025\">2025</a></div></div></div>');
