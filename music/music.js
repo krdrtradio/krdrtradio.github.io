d476f8ab17bf6a499905b33adf471edaf655f7e9
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-05-22 00:38:44 +0200

    Update music.js

--- music/music.js
+++ music/music.js
@@ -1,17 +1,1 @@
-; /*SIMPLE AUDIO PLAYER*/
-(() => {
-   // https://stackoverflow.com/a/34487069/383904
-   const AUD = new Audio(),
-      BTNS = document.querySelectorAll("[data-audio]");
-
-   function playPause() {
-      const src = this.dataset.audio;
-      if (AUD.src != src) AUD.src = src;
-      AUD[AUD.paused ? "play" : "pause"]();
-      BTNS.forEach(el => el.classList.remove("pause"));
-      this.classList.toggle("pause", !AUD.paused);
-   }
-
-   AUD.addEventListener("ended", playPause);
-   BTNS.forEach(el => el.addEventListener("click", playPause));
-})();
+(()=>{let e=new Audio,a=document.querySelectorAll("[data-audio]");function s(){let s=this.dataset.audio;e.src!=s&&(e.src=s),e[e.paused?"play":"pause"](),a.forEach(e=>e.classList.remove("pause")),this.classList.toggle("pause",!e.paused)}e.addEventListener("ended",s),a.forEach(e=>e.addEventListener("click",s))})();
