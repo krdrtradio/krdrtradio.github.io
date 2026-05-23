f94196b839f7b3a990e3e302cfe67f6188f6a3eb
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-05-23 17:52:28 +0200

    Update clubs.js

--- media/clubs.js
+++ media/clubs.js
@@ -1,1 +1,99 @@
-const jsonUrl="https://krdrtradio.github.io/media/json/clubs.json";let allClubs=[];async function loadClubs(){try{let e=await fetch("https://krdrtradio.github.io/media/json/clubs.json"),t=await e.json();allClubs=t.clubs,createCategories(allClubs),renderList(allClubs),document.getElementById("categoryFilter").addEventListener("change",filterClubs),document.getElementById("searchInputC").addEventListener("input",filterClubs)}catch(l){console.error("Błąd ładowania JSON:",l)}}function filterClubs(){let e=document.getElementById("categoryFilter").value,t=document.getElementById("searchInputC").value.toLowerCase(),l=allClubs.filter(l=>{let a="all"===e||l.category===e,n=l.name.toLowerCase().includes(t)||l.category.toLowerCase().includes(t);return a&&n});renderList(l)}function createCategories(e){let t=document.getElementById("categoryFilter"),l=[...new Set(e.map(e=>e.category))];l.forEach(e=>{let l=document.createElement("option");l.value=e,l.textContent=e,t.appendChild(l)})}function renderList(e){let t=document.getElementById("clubList");t.innerHTML="",e.forEach(e=>{let l=document.createElement("div");l.className="item";let a="";!0===e.audio&&(a=`<button onclick="playAudio('${e.value}', '${e.name}')">▶ Odtw\xf3rz</button>`),!1===e.audio&&(a=`<a href="${e.value}" target="_blank"><button>Otw\xf3rz w przeglądarce</button></a>`),l.innerHTML=`<div class="title">${e.name} |<span class="category">${e.category}</span></div>${a}`,t.appendChild(l)})}function playAudio(e,t){let l=document.getElementById("mainPlayer"),a=document.getElementById("mainSource"),n=document.getElementById("set-title");a.src=e,n.textContent=t,l.style.display="block",l.load(),l.play(),window.scrollTo({top:l.offsetTop-50,behavior:"smooth"})}loadClubs();
+const jsonUrl = 'https://krdrtradio.github.io/media/json/clubs.json';
+let allClubs = [];
+async function loadClubs() {
+   try {
+      const response = await fetch(jsonUrl);
+      const data = await response.json();
+      allClubs = data.clubs;
+      createCategories(allClubs);
+      renderList(allClubs);
+      // FILTR KATEGORII
+      document
+         .getElementById('categoryFilter')
+         .addEventListener('change', filterClubs);
+      // WYSZUKIWARKA
+      document
+         .getElementById('searchInputC')
+         .addEventListener('input', filterClubs);
+   } catch (error) {
+      console.error('Błąd ładowania JSON:', error);
+   }
+}
+
+function filterClubs() {
+   const category =
+      document.getElementById('categoryFilter').value;
+   const search =
+      document.getElementById('searchInputC')
+      .value
+      .toLowerCase();
+   const filtered = allClubs.filter(club => {
+      const matchCategory =
+         category === 'all' ||
+         club.category === category;
+      const matchSearch =
+         club.name.toLowerCase().includes(search) ||
+         club.category.toLowerCase().includes(search);
+      return matchCategory && matchSearch;
+   });
+   renderList(filtered);
+}
+
+function createCategories(clubs) {
+   const select = document.getElementById('categoryFilter');
+   const categories = [
+      ...new Set(clubs.map(club => club.category))
+   ];
+   categories.forEach(category => {
+      const option = document.createElement('option');
+      option.value = category;
+      option.textContent = category;
+      select.appendChild(option);
+   });
+}
+
+function renderList(clubs) {
+   const list = document.getElementById('clubList');
+   list.innerHTML = '';
+   clubs.forEach(club => {
+      const item = document.createElement('div');
+      item.className = 'item';
+      let button = '';
+      // audio=true → odtwarzaj w playerze
+      if (club.audio === true) {
+         button = `<button onclick="playAudio('${club.value}', '${club.name}')">▶ Odtwórz</button>`;
+      }
+      // audio=false → otwórz link
+      if (club.audio === false) {
+         button = `<a href="${club.value}" target="_blank"><button>Otwórz w przeglądarce</button></a>`;
+      }
+      item.innerHTML = `<div class="title">${club.name} | <span class="category">${club.category}</span></div>${button}`;
+
+      list.appendChild(item);
+   });
+}
+
+function playAudio(url, title) {
+
+   const player = document.getElementById('mainPlayer');
+   const source = document.getElementById('mainSource');
+   const settitle = document.getElementById('set-title');
+
+   source.src = url;
+
+   // tytuł seta
+   settitle.textContent = title;
+
+   // pokaż player
+   player.style.display = 'block';
+
+   player.load();
+   player.play();
+
+   // scroll do playera
+   window.scrollTo({
+      top: player.offsetTop - 50,
+      behavior: 'smooth'
+   });
+}
+loadClubs();
