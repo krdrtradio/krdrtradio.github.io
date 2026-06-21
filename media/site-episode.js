35bc8c2fa706d4f3415dd75f41180fdedef2d826
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-06-21 21:57:51 +0200

    Update site-episode.js

--- media/site-episode.js
+++ media/site-episode.js
@@ -739,7 +739,9 @@ async function loadAudioForPost(postId, mainUrl) {
         const proxyUrl = 'https://cors.krdrtradio.workers.dev/?url=';
 
         // Jeśli podcast jest CPT "podcast"
-        const postUrl = `${mainUrl}/wp-json/wp/v2/podcast/${postId}`;
+        const postUrl = mainUrl === 'radiokolor'
+            ? `https://radiokolor.pl/wp-json/wp/v2/podcast/${postId}`
+            : `${mainUrl}/wp-json/wp/v2/posts/${postId}`;
 
         const postRes = await fetch(
             proxyUrl + encodeURIComponent(postUrl)
