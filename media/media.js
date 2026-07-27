0543291a38f008add1bbfdc318b0573fa49c4998
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-07-28 01:11:10 +0200

    Rename podcast list leader class to functions

--- media/media.js
+++ media/media.js
@@ -206,7 +206,7 @@ function renderPeoples() {
                    <div class="podcast_list_name">
                        <a href="${url}" target="_blank">${escapeHTML(p.name)}</a>
                    </div>
-                   ${p.functions ? `<div class="podcast_list_leader">${Array.isArray(p.functions) ? escapeHTML(p.functions.join(', ')) : escapeHTML(p.functions)}</div>` : ""}
+                   ${p.functions ? `<div class="podcast_list_functions">${Array.isArray(p.functions) ? escapeHTML(p.functions.join(', ')) : escapeHTML(p.functions)}</div>` : ""}
                    ${p.leaders ? `<div class="podcast_list_host"><small>Prowadzi:</small> ${Array.isArray(p.leaders) ? escapeHTML(p.leaders.join(', ')) : escapeHTML(p.leaders)}</div>` : ""}
                </div>
            `;
