6c008eb2c2e6c4a1fcf229def8e45139aec3e75b
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-08-07 23:41:54 +0200

    Update people-index.js

--- media/people-index.js
+++ media/people-index.js
@@ -112,7 +112,7 @@ for (const item of allItems) {
             r.active &&
             !r.private &&
             !r.hide_in_schedule &&
-            (p.publish_from_date ? now >= new Date(p.publish_from_date))
+            (r.publish_from_date ? now >= new Date(r.publish_from_date))
         );
 
         for (const row of rows) {
