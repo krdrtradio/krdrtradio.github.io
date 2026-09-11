50adf4b85ec205be27aa9bd9a4b4e3013f1d6833
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-09-11 18:05:20 +0200

    Update site-article.js

--- media/site-article.js
+++ media/site-article.js
@@ -126,7 +126,7 @@ async function WPArticle(mainUrl, siteKey, is_categories = true, is_author = tru
       // Mapujemy tylko pojedyncze artykuły do stringa
       const articlesHtml = posts.map(post => {
          const author = post._embedded?.author?.[0];
-         const authorSite = mainUrl === "https://radiovictoria.pl" ? author?.link : `https://krdrtradio.github.io/media/article-list?si=${siteKey}&a=${author?.id}`;
+         const authorSite = `https://krdrtradio.github.io/media/article-list?si=${siteKey}&a=${author?.id}`;
          const authorHtml = author ? `<a href="${authorSite}">${author.name}</a>` : 'Redakcja';
 
          const terms = post._embedded?.['wp:term']?.[0] || [];
@@ -1131,9 +1131,7 @@ async function WPArticleList(
                   post._embedded.author[0];
 
                const link =
-                  mainUrl === "https://radiovictoria.pl"
-                     ? author.link
-                     : `https://krdrtradio.github.io/media/article-list?si=${siteKey}&a=${author.id}`;
+                  `https://krdrtradio.github.io/media/article-list?si=${siteKey}&a=${author.id}`;
 
                authorHTML =
                   `<a href="${link}">${author.name}</a>`;
@@ -1572,7 +1570,7 @@ async function WPArticlePost(slug, mainUrl, is_categories = true, is_tags = true
             const author = embed.author[0];
             const authorName = author.name || 'Redakcja';
             const authorId = author.id;
-            const authorSite = mainUrl === "https://radiovictoria.pl" ? author.link : `https://krdrtradio.github.io/media/article-list?si=${currentSiteKey}&a=${authorId}`;
+            const authorSite = `https://krdrtradio.github.io/media/article-list?si=${currentSiteKey}&a=${authorId}`;
             // Tworzymy link do profilu autora
             authorDisplay = `
                     <i class="fa-solid fa-user"></i> 
