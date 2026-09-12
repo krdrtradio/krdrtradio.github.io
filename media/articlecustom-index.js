e24135544f887e3c4924408f337614dc660b4431
krdrtradio <160354554+krdrtradio@users.noreply.github.com>
2026-09-12 13:45:21 +0200

    Update articlecustom-index.js

--- media/articlecustom-index.js
+++ media/articlecustom-index.js
@@ -1,1 +1,60 @@
-!function(){let r=new URLSearchParams(window.location.search),t=r.get("id"),i=r.get("si"),e=r.get("tp"),o=r.get("tc")||"categories",a={radiorsc:{url:"https://radiorsc.pl"},radiovictoria:{url:"https://radiovictoria.pl"},radiokolor:{url:"https://radiokolor.pl"},sosw:{url:"https://soswskierniewice.pl"},ckis:{url:"https://cekis.pl"},radiolodz:{url:"https://radiolodz.pl"},elradio:{url:"https://elradio.pl"},radiomaryja:{url:"https://radiomaryja.pl"}};if(!t||!i||!a[i]||!e){document.getElementById("article-post").innerHTML="Błąd: Nieprawidłowe parametry URL.";return}let{url:l}=a[i];window.addEventListener("load",function r(){try{if("function"==typeof WPCustomPost)WPCustomPost(t,l,e,o);else throw Error("Nie znaleziono funkcji ładującej.")}catch(i){console.error(i),document.getElementById("article-post").innerHTML="Błąd podczas ładowania modułu."}})}();
+(function () {
+   const params = new URLSearchParams(window.location.search);
+   const id = params.get('id');
+   const site = params.get('si');
+   const typename = params.get('tp');
+   const typecat = params.get('tc') || "categories";
+
+   const siteMap = {
+      "radiorsc": {
+         "url": "https://radiorsc.pl"
+      },
+      "radiovictoria": {
+         "url": "https://radiovictoria.pl"
+      },
+      "radiokolor": {
+         "url": "https://radiokolor.pl"
+      },
+      "sosw": {
+         "url": "https://soswskierniewice.pl"
+      },
+      "ckis": {
+         "url": "https://cekis.pl"
+      },
+      "radiolodz": {
+         "url": "https://radiolodz.pl"
+      },
+      "elradio": {
+         "url": "https://elradio.pl"
+      },
+      "radiomaryja": {
+         "url": "https://radiomaryja.pl"
+      },
+   };
+
+   if (!id || !site || !siteMap[site] || !typename) {
+      document.getElementById('article-post').innerHTML = "Błąd: Nieprawidłowe parametry URL.";
+      return;
+   }
+
+   const {
+      url: mainUrl
+   } = siteMap[site];
+
+   // Funkcja uruchamiająca odpowiedni moduł
+   function inicjuj() {
+      try {
+         if (typeof WPCustomPost === 'function') {
+            WPCustomPost(id, mainUrl, typename, typecat)
+         } else {
+            throw new Error("Nie znaleziono funkcji ładującej.");
+         }
+      } catch (e) {
+         console.error(e);
+         document.getElementById('article-post').innerHTML = "Błąd podczas ładowania modułu.";
+      }
+   }
+
+   // Czekamy na pełne załadowanie DOM i skryptów zewnętrznych
+   window.addEventListener('load', inicjuj);
+})();
